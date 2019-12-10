40b588ddd46217525df1ca8cf9c2e63a00e491fa
Tomasz Kaliciak <226543@student.pwr.edu.pl>
2019-12-10 01:16:16 +0100

    Dodano pole koleje

---
+++ Assets/Scripts/TrainField.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainField : Buyable
+{
+    private int resitPrice = 50;
+    public override void passThrough(Player player) { }
+
+    protected override int chargeForResit()
+    {
+        return resitPrice * getAmountOfRailwaysOwnedByOwner();
+    }
+    int getAmountOfRailwaysOwnedByOwner()
+    {
+        int numberOfRailwayOwnedByOwner = 0;
+        foreach (var railway in transform.parent.GetComponentsInChildren<TrainField>())
+        {
+            if (railway.getCurrentOwner() == owner)
+            {
+                ++numberOfRailwayOwnedByOwner;
+            }
+        }
+
+        return numberOfRailwayOwnedByOwner;
+    }
+}
